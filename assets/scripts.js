82745c335c6b05a07ebd73d81389b7ed92cae4da
Rouabah <rouabah.zineb@hotmail.fr>
2024-02-15 01:20:32 +0100

    ajouter le filtre des images au clic sur scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,50 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
+// Init Lightgallery
+const lgElement = document.getElementById('lightgallery')
+if (lgElement !== null) {
+  lightGallery(lgElement, {
+    // plugins: [lgZoom, lgThumbnail, lgShare, lgAutoplay, lgComment],
+    licenseKey: '0000-0000-000-0000',
+    speed: 500,
+    // allowMediaOverlap: true,
+    download: false,
+    mobileSettings: {
+      controls: false,
+      showCloseIcon: false,
+      download: false
+    }
+  })
+}
+// Init nav_Gallery
+document.addEventListener('DOMContentLoaded', () => {
+    const galButtons = document.querySelectorAll('#gallery button');
+    const galPictures = document.querySelectorAll('#gallery a');
+    const tousButton = document.getElementById('tousButton');
+    let selectedButton = tousButton;
+  
+    galButtons.forEach((button) => {
+      button.addEventListener('mouseup', () => {
+        const filter = button.getAttribute('data-filter');
+  
+        if (selectedButton) {
+          selectedButton.classList.remove('selected');
+        }
+  
+        button.classList.add('selected');
+        selectedButton = button;
+  
+        galPictures.forEach((picture) => {
+          const pictAttr = picture.getAttribute('data-tag');
+          if (filter === 'Tous' || pictAttr === filter) {
+            picture.style.display = 'block';
+          } else {
+            picture.style.display = 'none';
+          }
+        });
+  
+        console.log(`Filter applied: ${filter}`);
+      });
     });
-});
+  
+    console.log('Script JS loaded and executed.');
+  });
+  
